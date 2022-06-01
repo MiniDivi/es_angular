814c499dc7ea1e04bd44b0ad05e73d5dcb82c83c
lai2-01 <lai2-01@itismeucci.com>
2022-06-01 13:34:10 +0200

    primo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learn-angular';
+  data = [
+    {
+      "id": 10001,
+      "birthDate": "1953-09-01",
+      "firstName": "Georgi",
+      "lastName": "Facello",
+      "gender": "M",
+      "hireDate": "1986-06-25",
+    },
+    {
+      "id": 10002,
+      "birthDate": "1964-06-01",
+      "firstName": "Bezalel",
+      "lastName": "Simmel",
+      "gender": "F",
+      "hireDate": "1985-11-20",
+    },
+    {
+      "id": 10003,
+      "birthDate": "1959-12-02",
+      "firstName": "Parto",
+      "lastName": "Bamford",
+      "gender": "M",
+      "hireDate": "1986-08-27",
+    },
+    {
+      "id": 10004,
+      "birthDate": "1954-04-30",
+      "firstName": "Chirstian",
+      "lastName": "Koblick",
+      "gender": "M",
+      "hireDate": "1986-11-30",
+  
+    },
+    {
+      "id": 10005,
+      "birthDate": "1955-01-20",
+      "firstName": "Kyoichi",
+      "lastName": "Maliniak",
+      "gender": "M",
+      "hireDate": "1989-09-11",
+  
+    }
+  ];
+
+  dipendente = false;
+  datiDipendente = {
+    "firstName": '',
+    "lastName": ''
+  };
+
+
+  modifica(i:Object){    
+    this.dipendente = true;
+    this.datiDipendente = JSON.stringify(i);
+    console.log(this.datiDipendente.firstName);
+  }
+  
 }
+  
