6af27a3dd023ea9b2d7643e8b1268dc37622994f
lai2-01 <lai2-01@itismeucci.com>
2022-06-08 13:45:58 +0200

    Tabella con Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,27 @@
-import { Component, Input } from '@angular/core';
-import { Employee } from './Employee';
+import {Component} from '@angular/core';
 
+export interface Employee {
+  id: number,
+  birthDate: string,
+  firstName: string,
+  lastName: string,
+  gender: string,
+  hireDate: string,
+}
+
+const ELEMENT_DATA: Employee[] = [
+  {id: 10001, birthDate: '1953-09-01', firstName: 'Georgi', lastName: 'Facello', gender: 'M', hireDate: '1986-06-25'},
+  {id: 10002, birthDate: '1964-06-01', firstName: 'Bezalel', lastName: 'Simmel', gender: 'F', hireDate: '1985-11-20'},
+  {id: 10003, birthDate: '1959-12-02', firstName: 'Parto', lastName: 'Bamford', gender: 'M', hireDate: '1986-08-27'},
+  {id: 10004, birthDate: '1954-04-30', firstName: 'Chirstian', lastName: 'Koblick', gender: 'M', hireDate: '1986-11-30'},
+  {id: 10005, birthDate: '1955-01-20', firstName: 'Kyoichi', lastName: 'Maliniak', gender: 'M ', hireDate: '1989-09-11'}
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data = [
-    {
-      "id": 10001,
-      "birthDate": "1953-09-01",
-      "firstName": "Georgi",
-      "lastName": "Facello",
-      "gender": "M",
-      "hireDate": "1986-06-25",
-    },
-    {
-      "id": 10002,
-      "birthDate": "1964-06-01",
-      "firstName": "Bezalel",
-      "lastName": "Simmel",
-      "gender": "F",
-      "hireDate": "1985-11-20",
-    },
-    {
-      "id": 10003,
-      "birthDate": "1959-12-02",
-      "firstName": "Parto",
-      "lastName": "Bamford",
-      "gender": "M",
-      "hireDate": "1986-08-27",
-    },
-    {
-      "id": 10004,
-      "birthDate": "1954-04-30",
-      "firstName": "Chirstian",
-      "lastName": "Koblick",
-      "gender": "M",
-      "hireDate": "1986-11-30",
-  
-    },
-    {
-      "id": 10005,
-      "birthDate": "1955-01-20",
-      "firstName": "Kyoichi",
-      "lastName": "Maliniak",
-      "gender": "M",
-      "hireDate": "1989-09-11",
-  
-    }
-  ];
-
-  dipendente = false;
-  datiDipendente: Employee | null = null;
-
-
-  modifica(el : Employee){    
-    this.datiDipendente = el;
-  }
-
-  elimina(id:number){
-    for(let i = 0; i < this.data.length; i++){
-      if(this.data[i].id == id){
-        this.data.splice(i, 1);
-      }
-    }
-  }
-  
+  displayedColumns: string[] = ['id', 'birthDate', 'firstName', 'lastName', 'gender', 'hireDate'];
+  dataSource = ELEMENT_DATA;
 }
-  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
